f977d83ce4aebafc5758ad9db5ea4c98f405fd1f
tanjunbin <tan73365625@qq.com>
2019-04-28 09:39:04 +0800

    Update DateUtils.java
    根据日期yyyy-MM-dd 获取星期几

--- lib_utils_android/src/main/java/com/tan/lib_utils_android/common/DateUtils.java
+++ lib_utils_android/src/main/java/com/tan/lib_utils_android/common/DateUtils.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -182,4 +183,28 @@ public class DateUtils {
         }
         return 0;
     }
+
+    /**
+     * 根据日期yyyy-MM-dd 获取星期几
+      * @param sdate
+     * @return
+     */
+    public static String getWeek(String sdate) {
+        // 再转换为时间
+        Date date = strToDate(sdate);
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        // int hour=c.get(Calendar.DAY_OF_WEEK);
+        // hour中存的就是星期几了，其范围 1~7
+        // 1=星期日 7=星期六，其他类推
+        return new SimpleDateFormat("EEEE").format(c.getTime());
+    }
+
+    public static Date strToDate(String strDate) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        ParsePosition pos = new ParsePosition(0);
+        Date strtodate = formatter.parse(strDate, pos);
+        return strtodate;
+    }
+
 }
