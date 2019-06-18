f5d7b0ee7cbaab9c0d4d3adcd1d4060bda14e852
tanjunbin <tan73365625@qq.com>
2019-06-18 14:00:40 +0800

    添加手机状态栏,全透明,半透明等

--- lib_utils_android/src/main/java/com/tan/lib_utils_android/app/AppUtils.java
+++ lib_utils_android/src/main/java/com/tan/lib_utils_android/app/AppUtils.java
@@ -7,12 +7,22 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.widget.DrawerLayout;
 import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class AppUtils {
+
+    private static int statusHeight = 0;
+    private View contentViewGroup;
 
     /**
      * 获取应用程序名称
@@ -50,7 +60,6 @@ public class AppUtils {
         return null;
     }
 
-
     /**
      * 获取应用程序版本名称信息
      * versionCode
@@ -70,7 +79,6 @@ public class AppUtils {
         return 0;
     }
 
-
     /**
      * 获取应用程序版本名称信息
      * AndroidManifest package
@@ -89,7 +97,6 @@ public class AppUtils {
         }
         return null;
     }
-
 
     /**
      * 获取图标 bitmap
@@ -111,6 +118,19 @@ public class AppUtils {
         return bd.getBitmap();
     }
 
+    public static int getStatusHeight(Context context) {
+        if (statusHeight <= 0) {
+            try {
+                Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+                Object object = clazz.newInstance();
+                int height = Integer.parseInt(clazz.getField("status_bar_height").get(object).toString());
+                statusHeight = context.getResources().getDimensionPixelSize(height);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return statusHeight;
+    }
 
     /**
      * 启动到应用商店app详情界面
@@ -141,6 +161,80 @@ public class AppUtils {
             activity.startActivity(intent);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * 全透状态栏
+     */
+    public void setStatusBarFullTransparent(Activity activity) {
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            Window window = activity.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 半透明状态栏
+     */
+    public void setHalfTransparent(Activity activity) {
+
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            View decorView = activity.getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 如果需要内容紧贴着StatusBar
+     * 应该在对应的xml布局文件中，设置根布局
+     * android:fitsSystemWindows="true"
+     * android:clipToPadding="false"
+     * <p>
+     * 子布局设置android:fitsSystemWindows="true"
+     */
+    public void setFitSystemWindow(Activity activity, boolean fitSystemWindow) {
+        if (contentViewGroup == null) {
+            contentViewGroup = ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+        }
+        contentViewGroup.setFitsSystemWindows(fitSystemWindow);
+    }
+
+    /**
+     * 为了兼容4.4的抽屉布局->透明状态栏
+     */
+    public void setDrawerLayoutFitSystemWindow(Activity activity) {
+        if (Build.VERSION.SDK_INT == 19) {//19表示4.4
+            int statusBarHeight = getStatusHeight(activity);
+            if (contentViewGroup == null) {
+                contentViewGroup = ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+            }
+            if (contentViewGroup instanceof DrawerLayout) {
+                DrawerLayout drawerLayout = (DrawerLayout) contentViewGroup;
+                drawerLayout.setClipToPadding(true);
+                drawerLayout.setFitsSystemWindows(false);
+                for (int i = 0; i < drawerLayout.getChildCount(); i++) {
+                    View child = drawerLayout.getChildAt(i);
+                    child.setFitsSystemWindows(false);
+                    child.setPadding(0, statusBarHeight, 0, 0);
+                }
+
+            }
         }
     }
 }
